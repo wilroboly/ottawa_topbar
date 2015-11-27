0b524495603483ce81593c3e644bf2fa1dc05074
Wil Roboly <wilco@nurf.com>
2015-11-27 01:34:42 -0500

    Updated path to logo image in topbar template.

--- templates/ottawa-logo.tpl.php
+++ templates/ottawa-logo.tpl.php
@@ -1,3 +1,6 @@
 <div class="ottawa-logo-container navbar-nav navbar-left">
-  <a class="ottawa-logo-link" href="http://www.ottawa.ca"><img class="ottawa-logo" src="/sites/all/themes/ottawa_bootstrap_sass/images/logo-cityofottawa-white.png"></a>
+  <a class="ottawa-logo-link" href="http://www.ottawa.ca"><img
+      class="ottawa-logo"
+      src="/profiles/ottawa_profile/themes/contrib/ottawa_bootstrap_sass/images/logo-cityofottawa-white.png"></a>
 </div>
+
