372da1c6c01f5d032bcc49051d449384281aa264
Wil Roboly <wilco@nurf.com>
2016-01-20 15:16:36 -0500

    Fix bad image file path

--- templates/ottawa-logo.tpl.php
+++ templates/ottawa-logo.tpl.php
@@ -1,6 +1,6 @@
 <div class="ottawa-logo-container navbar-nav navbar-left">
   <a class="ottawa-logo-link" href="http://www.ottawa.ca"><img
       class="ottawa-logo"
-      src="/profiles/ottawa_profile/themes/contrib/ottawa_bootstrap_sass/images/logo-cityofottawa-white.png"></a>
+      src="/profiles/ottawa_profile/themes/custom/ottawa_bootstrap_sass/images/logo-cityofottawa-white.png"></a>
 </div>
 
